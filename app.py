63d2a8d199bfccec4df413be5fcf7d3c2387cda3
Rehjii-Martin <rehjiimartin15@gmail.com>
2025-08-04 14:29:01 -0700

    - Added note in app.py regarding setting reload to False for production as reload feature in Uvicorn is not stable nor safe for live production, only safe for development.

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ app.include_router(auth_router)
 # `python app.py`
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) # dev: reload on code changes, set reload=False for production
